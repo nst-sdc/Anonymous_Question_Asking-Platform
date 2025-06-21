d79c988bbc24a22596410523ed3ce16154e3617d
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-06-21 08:04:42 +0530

    add ToastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
 import { SocketProvider } from './context/SocketContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import AuthScreen from './component/AuthScreen';
 import TeacherDashboard from './component/TeacherDashboard';
@@ -11,11 +13,25 @@ function App() {
     <AppProvider>
       <SocketProvider>
         <Router>
-          <Routes>
-            <Route path="/" element={<AuthScreen />} />
-            <Route path="/teacher" element={<TeacherDashboard />} />
-            <Route path="/student" element={<StudentDashboard />} />
-          </Routes>
+          <>
+            <Routes>
+              <Route path="/" element={<AuthScreen />} />
+              <Route path="/teacher" element={<TeacherDashboard />} />
+              <Route path="/student" element={<StudentDashboard />} />
+            </Routes>
+            <ToastContainer
+              position="top-right"
+              autoClose={3000}
+              hideProgressBar={false}
+              newestOnTop
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="light"
+            />
+          </>
         </Router>
       </SocketProvider>
     </AppProvider>

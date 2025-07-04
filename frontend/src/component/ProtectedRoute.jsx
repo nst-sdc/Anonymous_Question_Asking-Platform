4318f0d3d3b7d6ca1b4f3134dc7cac16a53871e1
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-07-04 18:32:58 +0530

    add comment

--- frontend/src/component/ProtectedRoute.jsx
+++ frontend/src/component/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React, { Children } from "react";
 import { Navigate } from "react-router-dom";
 import { useApp } from '../context/AppContext';
 
+// Prevent access of user if user is unauthenticated or user is unauthorized
 const ProtectedRoute = ({children, allowedRole}) => {
   const { user } = useApp();
 

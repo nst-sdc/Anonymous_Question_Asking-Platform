362d0bef780eb96cb6fa149848724389391fdba1
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-07-11 10:56:51 +0530

    import x from lucide-react

--- frontend/src/component/PollForm.jsx
+++ frontend/src/component/PollForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { X } from 'lucide-react';
 
 const PollForm = ({setShowPollForm, pollQuestion, setPollQuestion, pollOptions,setPollOptions, handleCreatePoll}) => {
   return (
